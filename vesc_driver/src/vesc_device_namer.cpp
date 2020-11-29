f296be06a9d30daacf26dcfcba56951bc4024230
Andrea Scipione <andrea.scipione@gmail.com>
2020-11-29 08:32:41 +0100

    regola assegnazione nome tramite uuid

--- vesc_driver/src/vesc_device_namer.cpp
+++ vesc_driver/src/vesc_device_namer.cpp
@@ -8,7 +8,7 @@ int main(int argc, char** argv)
    std::string devicePort=   argv[1];
    std::string VESC_UUID_ENV="VESC_UUID_ENV=";
 
-   vesc_driver::VescDeviceLookup lookup(devicePort);
+   vesc_driver::VescDeviceLookup lookup("/dev/"+devicePort);
 
    for(int i=0;i<50;i++){
       usleep(20);
@@ -19,7 +19,8 @@ int main(int argc, char** argv)
     if(lookup.isReady())
    {
       VESC_UUID_ENV += lookup.deviceUUID(); 
-      std::cout << VESC_UUID_ENV <<std::endl;
+      //std::cout << VESC_UUID_ENV <<std::endl;
+      std::cout<<lookup.deviceUUID();
       putenv( const_cast<char*>(VESC_UUID_ENV.c_str()) );
       return 0;
    } else return -1;
